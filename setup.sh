07924e59a91a15f85825b0bbde546bdb0980df28
Alexander Morton <amorton@lxplus065.cern.ch>
2016-04-15 15:33:48 +0200

    Add setup before compilation

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#source /afs/phas.gla.ac.uk/system/sl6x/x86_64/gcc/4.8.2/setup.sh
+
+#export C=/afs/phas.gla.ac.uk/system/sl6x/x86_64/gcc/4.8.2/bin/g++
+#and for CXX
+
+#export ROOTSYS=/data/ppe01/sl6x/x86_64/root/5.34.00
+#///////
+#ls /afs/cern.ch/sw/lcg/external/gcc/4.8.1/i686-slc6-gcc48-opt/setup.sh
+
+source  /afs/cern.ch/sw/lcg/contrib/gcc/4.7.1/x86_64-slc6/setup.sh
+export CXX=/afs/cern.ch/sw/lcg/contrib/gcc/4.7.1/x86_64-slc6/bin/g++
+export C=/afs/cern.ch/sw/lcg/contrib/gcc/4.7.1/x86_64-slc6/bin/g++
+#export CXX=/afs/cern.ch/sw/lcg/contrib/gcc/4.7.2/x86_64-slc6/bin/g++
+#export C=/afs/cern.ch/sw/lcg/contrib/gcc/4.7.2/x86_64-slc6/bin/g++
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.26/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+
+
+
+#setupATLAS 
+#lSetupROOT
